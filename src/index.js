5e756de39fd9d50631636474e2a3fc09a9cb41e3
Ruben Garcia <rgarcia@beyond.ai>
2022-07-05 12:15:36 +0300

    Log app intialization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+const app = require('./app')
+const port = app.get('port')
+const server = app.listen(port)
 
 process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+  console.error('Unhandled Rejection at: Promise ', p, reason)
+)
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+  console.info(
+    'Feathers application started on http://%s:%d',
+    app.get('host'),
+    port
+  )
+)
